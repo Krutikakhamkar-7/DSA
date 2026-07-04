eb389c7bb3e3b863bacbad2ea100a93f42ff20ae
Krutikakhamkar-7 <krutikakhamkar07@gmail.com>
2026-07-04 17:13:08 +0530

    Solved Valid Parentheses

---
+++ Arrays/ValidParentheses.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for(char ch : s)
+        {
+            if(ch == '(' || ch == '{' || ch == '[')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+
+                if((ch == ')' && st.top() != '(') ||
+                   (ch == '}' && st.top() != '{') ||
+                   (ch == ']' && st.top() != '['))
+                {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    string s = "()[]{}";
+
+    if(obj.isValid(s))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
